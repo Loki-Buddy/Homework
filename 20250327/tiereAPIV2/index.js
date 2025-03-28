4650800e60f1d458857674700329a010eadf1d25
Loki-Buddy <jm.conring@live.de>
2025-03-28 12:55:40 +0100

    aktuell

--- 20250327/tiereAPIV2/index.js
+++ 20250327/tiereAPIV2/index.js
@@ -13,3 +13,24 @@ function writeFile(data){
     fs.writeFileSync("tiere.json", JSON.stringify(data, null, 2));
 }
 
+app.get("/tiere", (req,res) => {
+    const tiere = readFile();
+    res.json(tiere);
+    /* res.send(tiere); */
+});
+
+app.post("/tiere", (req,res) =>{
+    const tiere = readFile();
+    const {name, art} = req.body;
+
+    const newTier = {
+        id: tiere.length > 0 ? Math.max(...tiere.map(tier => tier.id)) + 1 : 1,
+        name,
+        art
+    }
+    tiere.push(newTier);
+    writeFile(tiere);
+    res.status(201).json(newTier);
+});
+
+app.listen(3001);
